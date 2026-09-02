a3e6935f3bc878f01c378dba832dbe3869a26158
annajulcorrea09 <annajulcorrea09@gmail.com>
2026-09-02 15:03:06 -0300

    this girl is on fireeeee

--- GMusic/screens/MusicPlayer.js
+++ GMusic/screens/MusicPlayer.js
@@ -15,7 +15,7 @@ import {
   View
 } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { songs } from '../model/data';
+import songs  from '../model/data';
 import colors from '../theme/colors';
 
 const audioSources = songs.map((song) => song.url);
@@ -97,12 +97,12 @@ export default function MusicPlayer() {
 
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.header}>
+      {/* <View style={styles.header}>
         <Text style={styles.eyebrow}>TOCANDO AGORA</Text>
         <Text style={styles.counter}>
           {selectedIndex + 1} de {songs.length}
         </Text>
-      </View>
+      </View> */}
 
       <FlatList 
         data={songs}
@@ -118,6 +118,19 @@ export default function MusicPlayer() {
         <Text style={styles.songTitle}>{currentSong.title}</Text>
         <Text style={styles.songArtist}>{currentSong.artist}</Text>
       </View>
+
+      <Pressable
+       disabled={!status.isLoaded}
+       onPress={handlePlayPause}
+       style={styles.playButton}
+      > 
+      <Ionicons
+        name={status.playing ? 'pause' : 'play'}
+        size={38}
+        color={colors.background}
+      />
+      </Pressable>
+    
     </SafeAreaView>
   )
 }
@@ -183,5 +196,13 @@ const styles = StyleSheet.create({
     marginTop: 6,
     color: colors.textSecondary,
     fontSize: 14,
+  },
+  playButton:{
+    width: 78,
+    height: 78,
+    borderRadius: 39,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: colors.primary,
   }
 })
