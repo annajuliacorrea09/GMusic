927dabf3e1b7b0a000516e3de389fdf048edcb21
annajulcorrea09 <annajulcorrea09@gmail.com>
2026-09-22 12:23:42 -0300

    war is (almost) over

--- GMusic/utils/formatTime.js
+++ GMusic/utils/formatTime.js
@@ -7,5 +7,5 @@ export default function formatTime(seconds) {
   const minutes = Math.floor(totalSeconds / 60);
   const remainingSeconds = totalSeconds % 60;
 
-  return `${String(minutes).padStart(2,'0')}:${String(remainingSeconds).padStart(2,'0')}`;
+  return `${String(minutes).padStart(2, '0')}:${String(remainingSeconds).padStart(2, '0')}`;
 }
